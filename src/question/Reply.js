f0574d02f126a0138579e08aab1d4ebdd34102b1
jkthomas <tom.januszek@gmail.com>
2018-11-20 20:45:51 +0100

    [INPUT] Added input type management and saving

--- src/question/Reply.js
+++ src/question/Reply.js
@@ -1,11 +1,21 @@
 import React, {Component} from "react";
 
 class Reply extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            checkedOption: "0"
+        }
+    }
     render() {
+        const inputType = "radio";
+        let index = "0";
         return (
-            <li>
-                {this.props.reply.content}
-            </li>)
+            <form>
+                <input type={inputType} value={index}/>
+                {this.props.reply.content}<br />
+            </form>
+            )
     }
 }
 
--- src/question/ReplyManager.js
+++ src/question/ReplyManager.js
@@ -1,15 +1,41 @@
 import React, {Component} from "react";
-import Reply from "./Reply";
 
 class ReplyManager extends Component {
+    // noinspection JSMethodCanBeStatic
+    getInputType(type) {
+        switch (type) {
+            case 1:
+                return "radio";
+            case 2:
+                return "radio";
+            case 3:
+                return "text";
+            case 4:
+                return "checkbox";
+            default:
+                return "";
+        }
+    }
+
     render() {
-        const replies = this.props.replies.map((reply) =>
-            <Reply key={reply.id} reply={reply}/>
+        this.inputType = "";
+        this.inputType = this.getInputType(this.props.replies[0].type);
+        const replies = this.props.replies.map((reply) => {
+                return ([
+                        <input key={reply.id} type={this.inputType}/>,
+                        <label>{reply.content}</label>,
+                        <br/>
+                    ]
+                )
+            }
         );
         return (
             <div>
                 {replies}
-                <button onClick={() => {this.props.buttonClick()}}>Next question</button>
+                <button onClick={() => {
+                    this.props.buttonClick()
+                }}>Next question
+                </button>
             </div>)
     }
 }
